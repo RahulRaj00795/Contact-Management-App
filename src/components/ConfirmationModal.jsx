81119bf84e805da09be8b39d9a1677103fa58017
Rahul Raj <103984770+RahulRaj00795@users.noreply.github.com>
2025-01-21 15:28:19 +0530

    Update ConfirmationModal.jsx

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+//confirmation modal
 const ConfirmationModal = ({ name, onConfirm, onCancel }) => {
     return (
         <div className="modal">
             <p>Are you sure you want to delete the record of {name}?</p>
             <button onClick={onConfirm}>Delete</button>
             <button onClick={onCancel}>No, Cancel</button>
+            
         </div>
     );
 };
